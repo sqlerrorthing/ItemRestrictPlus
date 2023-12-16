1fa153422d860731d38a1795d0d47fa2c1e773de
oneqxz <oneqxz@gmail.com>
2023-12-17 02:18:23 +0800

    first

---
+++ src/main/java/me/oneqxz/itemrestrictplus/ItemRestrictPlus.java
@@ -0,0 +1,17 @@
+package me.oneqxz.itemrestrictplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemRestrictPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
